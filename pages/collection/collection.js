c7db33d2575175aebe6034fdc9240f32ddc315c8
ShanRang <wdd625629@163.com>
2019-08-16 09:16:23 +0800

    wangdd20190814

--- pages/collection/collection.js
+++ pages/collection/collection.js
@@ -13,6 +13,20 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
     var queryPath = 'querycollect/';
     wx.request({
       url: getApp().globalData.urlPath + queryPath,
@@ -29,20 +43,6 @@ Page({
         })
       }
     }) 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
   },
 
   /**

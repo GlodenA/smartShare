07a3ca2dc68c0e4d8da5ad0e8d031c49a66cd71e
GoldenA <fsq@890312>
2019-08-23 16:58:45 +0800

    删除IDEA配置文件

--- app.js
+++ app.js
@@ -7,12 +7,11 @@ App({
     // wx.setStorageSync('logs', logs)
 
     //登录
-  
+
     wx.checkSession({
       success() {
         //session_key 未过期，并且在本生命周期一直有效
-        if (!wx.getStorageSync("cookie")) 
-        {
+        if (!wx.getStorageSync("cookie")) {
           wx.login({
             success: res => {
               console.log("code:" + res.code);
@@ -28,10 +27,10 @@ App({
                 },
                 success: res => {
                   wx.setStorageSync('cookie', res.data.data.USER_ID),
-                  wx.setStorageSync('ntacct', res.data.data.NTACCT),
-                  // getApp().globalData.avatarUrl = res.data.data.PORTRAIT,
-                  // getApp().globalData.nickName = res.data.data.NI_NAME,
-                  getApp().globalData.userId = res.data.data.USER_ID
+                    wx.setStorageSync('ntacct', res.data.data.NTACCT),
+                    // getApp().globalData.avatarUrl = res.data.data.PORTRAIT,
+                    // getApp().globalData.nickName = res.data.data.NI_NAME,
+                    getApp().globalData.userId = res.data.data.USER_ID
                   // getApp().globalData.username = res.data.data.USER_NAME,
                   // getApp().globalData.email = res.data.data.EMAIL,
                   // getApp().globalData.phone=res.data.data.PHONE,
@@ -46,24 +45,24 @@ App({
       },
       fail() {
         // session_key 已经失效，需要重新执行登录流程
-        wx.login({ 
+        wx.login({
           success: res => {
             console.log("code:" + res.code);
             // 发送sessionId  res.code 到后台换取 openId, sessionKey, unionId，并存在服务端
             wx.request({
-              url: getApp().globalData.urlPath+'user/login',
+              url: getApp().globalData.urlPath + 'user/login',
               data: {
                 code: res.code,
-              },  
+              },
               header: {
                 'content-type': 'application/json' // 默认值
-              }, 
-              success:res=>{
+              },
+              success: res => {
                 wx.setStorageSync('cookie', res.data.data.USER_ID),
-                wx.setStorageSync('ntacct', res.data.data.NTACCT),
-                // getApp().globalData.avatarUrl = res.data.data.PORTRAIT,
-                // getApp().globalData.nickName = res.data.data.NI_NAME
-                getApp().globalData.userId = res.data.data.USER_ID
+                  wx.setStorageSync('ntacct', res.data.data.NTACCT),
+                  // getApp().globalData.avatarUrl = res.data.data.PORTRAIT,
+                  // getApp().globalData.nickName = res.data.data.NI_NAME
+                  getApp().globalData.userId = res.data.data.USER_ID
                 // getApp().globalData.username = res.data.data.USER_NAME,
                 // getApp().globalData.email = res.data.data.EMAIL,
                 // getApp().globalData.phone = res.data.data.PHONE,
@@ -82,16 +81,16 @@ App({
       success: (res) => {
         this.globalData.screenHeight = res.screenHeight
       }
-    }) 
+    })
   },
   globalData: {
-    userId:"",
-    avatarUrl:"",
-    nickName:"",
-    ntacct:'',
-    screenHeight:'',
+    userId: "",
+    avatarUrl: "",
+    nickName: "",
+    ntacct: '',
+    screenHeight: '',
     urlPath: 'https://www.bainiu6.com/docs/',
-    //urlPath: 'http://192.168.1.16:9001/docs/',
+    // urlPath: 'http://192.168.1.16:9001/docs/',
     //urlPath: 'http://127.0.0.1:9001/docs/',
   },
 })
